26600fdf9e9c1d426a5a87f02cdea1374f13e4b3
AndresPinzon <andrespinzonxd@gmail.com>
2024-09-09 17:45:12 -0500

    feat(front-end): Change navbar

--- ie-front-end-with-react/src/components/navbar/NavBar.jsx
+++ ie-front-end-with-react/src/components/navbar/NavBar.jsx
@@ -1,55 +1,100 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
 import { makeStyles } from '@mui/styles';
-
-
+import LinkBehavior from './LinkBehavior'; // Asegúrate de importar el componente personalizado
+import normalizeText from '../../utils/textUtils'; // Importación de la función de normalización de texto
+import logoSena from '../../assets/images/logoSena.png'; // Verifica que la ruta a la imagen sea correcta
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-        flexGrow: 1,
-    },
-    link: {
-        color: 'white',
-        textDecoration: 'none',
-        margin: theme.spacing(2),
-    },
-    toolbar: {
-        display: 'flex',
-        justifyContent: 'space-between',
-    }
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    minHeight: '64px',
+  },
+  logo: {
+    height: '40px',
+    width: '40px',
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  link: {
+    color: 'white',
+    textDecoration: 'none',
+    margin: theme.spacing(2),
+  },
+  drawer: {
+    width: 250,
+  },
+  drawerLink: {
+    textDecoration: 'none',
+    color: theme.palette.text.primary,
+  },
 }));
 
-export default function NavBar() {
-    const classes = useStyles();
-  
-    return (
-      <AppBar position='static'>
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" className={classes.title}>
-            SERVICIO NACIONAL DE APRENDIZAJE - SENA
-          </Typography>
-          <div>
-            <Link to="/home" className={classes.link}>
-              <Button color="inherit">Home</Button>
-            </Link>
-            <Link to="/cursos" className={classes.link}>
-              <Button color="inherit">Cursos</Button>
-            </Link>
-            <Link to="/usuarios" className={classes.link}>
-              <Button color="inherit">Usuarios</Button>
-            </Link>
-            <Link to="/masinformacion" className={classes.link}>
-              <Button color="inherit">Más Información</Button>
-            </Link>
-            <Link to="/login" className={classes.link}>
-              <Button color="inherit">Login</Button>
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-    );
+export default function Navbar() {
+  const classes = useStyles();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const drawer = (
+    <div className={classes.drawer}>
+      <List>
+        {['Home', 'Cursos', 'Usuarios', 'Más Información'].map((text) => (
+          <ListItem button component={LinkBehavior} to={`/${normalizeText(text)}`} key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <>
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        <img src={logoSena} alt="SENA Logo" className={classes.logo} />
+        <Typography variant="h6" className={classes.title}>
+          SERVICIO NACIONAL DE APRENDIZAJE SENA
+        </Typography>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleDrawerToggle}
+          sx={{ display: { xs: 'block', md: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          {['Home', 'Cursos', 'Usuarios', 'Más Información'].map((text) => (
+            <Button color="inherit" component={LinkBehavior} to={`/${normalizeText(text)}`} key={text}>
+              {text}
+            </Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+    <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerToggle}>
+      {drawer}
+    </Drawer>
+
+    </>
+    
+  );
 }
